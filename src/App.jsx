82d3f2e6ca7e02f1fe10bb47acc039e41845ba7a
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2024-09-09 14:02:36 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Pokemon from './components/Pokemon'
+
+function App() {
+
+  return (
+   <div>
+<h1>Index</h1>
+<Pokemon />
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/TypeSelection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TypeSelection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TypeSelection
